de6dbe0456a45e6a2330e73182a91e6cbcf21222
Yishai Zehavi <zehaviyishai@gmail.com>
2020-04-06 23:38:16 +0300

    test: wrote unit tests for edit task case

--- test/main.spec.js
+++ test/main.spec.js
@@ -7,6 +7,7 @@ import { Main } from '../src/js/main';
 setup(async function() {
     const dom = await JSDOM.fromFile(resolve(__dirname, '../src/index.html'));
     globalThis.document = dom.window.document;
+    globalThis.MouseEvent = dom.window.MouseEvent;
     this.main = new Main();
 });
 
@@ -26,7 +27,7 @@ test('when clicking the createNewTaskButton new item appended to taskList', func
     expect(this.main.tasksList.children).to.have.lengthOf(1);
 });
 
-test('when clicking the status label of and item, state.toggleTaskState() should be called', function() {
+test('when clicking the status label of an item, state.toggleTaskState() should be called', function() {
     spy(this.main.state);
 
     this.main.createNewTaskButton.click();
@@ -34,4 +35,49 @@ test('when clicking the status label of and item, state.toggleTaskState() should
     document.querySelector('li label').click();
 
     expect(this.main.state.toggleTaskState).to.be.called;
+});
+
+test('when double clicking on the item\'s text, the edit input should have class `active` and editMode() should be called', function() {
+    spy(this.main);
+
+    this.main.createNewTaskButton.click();
+
+    document.querySelector('li .text span').dispatchEvent(new MouseEvent('dblclick'));
+
+    expect(this.main.editMode).to.be.called;
+    expect(document.querySelector('li .text input')).to.have.property('className').include('active');
+});
+
+test('when double clicking on the item\'s edit input, the edit input should not have class `active` and displayMode() should be called', function() {
+    spy(this.main);
+
+    this.main.createNewTaskButton.click();
+
+    document.querySelector('li .text span').dispatchEvent(new MouseEvent('dblclick'));
+    document.querySelector('li .text input').dispatchEvent(new MouseEvent('dblclick'));
+
+    expect(this.main.displayMode).to.be.called;
+    expect(document.querySelector('li .text input')).property('className').not.include('active');
+});
+
+test('when double clicking on the item\'s edit input, state.updateTaskContent() should be called', function() {
+    spy(this.main.state);
+
+    this.main.createNewTaskButton.click();
+
+    document.querySelector('li .text span').dispatchEvent(new MouseEvent('dblclick'));
+    document.querySelector('li .text input').dispatchEvent(new MouseEvent('dblclick'));
+
+    expect(this.main.state.updateTaskContent).to.be.called;
+});
+
+test('when clicking the status label of an item while on "edit mode", displayMode() should be called', function() {
+    spy(this.main);
+
+    this.main.createNewTaskButton.click();
+
+    document.querySelector('li .text span').dispatchEvent(new MouseEvent('dblclick'));
+    document.querySelector('li label').click();
+
+    expect(this.main.displayMode).to.be.called;
 });
