56b65ddb6c0aa739bb82a3cfd7916680a5b7641a
Yishai Zehavi <zehaviyishai@gmail.com>
2020-04-07 10:51:33 +0300

    test: wrote an e2e test for adding multiple tasks case

---
+++ cypress/integration/add-multiple-tasks-e2e.spec.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+it('should be able multiple tasks and control them independently', () => {
+    cy.visit('http://localhost:1234');
+
+    cy.get('.input > input').type('First task');
+    cy.get('.btn').click();
+
+    cy.get('.input > input').type('Second task');
+    cy.get('.btn').click();
+
+    // control state
+    cy.get('.tasks ul li').first().find('label').click();
+    cy.get('.tasks ul li').first().find('input[type="checkbox"]').should('be.checked');
+    cy.get('.tasks ul li').eq(1).find('input[type="checkbox"]').should('not.be.checked');
+
+    // control content editing
+    cy.get('.tasks ul li').eq(1).find('.text span').dblclick();
+    cy.get('.tasks ul li').first().find('.text input').should('not.be.visible');
+    cy.get('.tasks ul li').eq(1).find('.text input').should('be.visible');
+    cy.get('.tasks ul li').eq(1).find('.text input').type('{selectall}Changed').dblclick();
+    cy.get('.tasks ul li').eq(1).find('.text span').should('have.text', 'Changed');
+    cy.get('.tasks ul li').first().find('.text span').should('have.text', 'First task');
+
+    cy.end();
+});
