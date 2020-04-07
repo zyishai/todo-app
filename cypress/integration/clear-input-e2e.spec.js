827339842b8d8304c536d3c576ec74b85bf352ee
Yishai Zehavi <zehaviyishai@gmail.com>
2020-04-07 10:59:19 +0300

    test: wrote e2e test for clearing the input after form submittion case

---
+++ cypress/integration/clear-input-e2e.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+it('after submitting the "add task" form, the input is cleared', () => {
+    cy.visit('http://localhost:1234');
+
+    cy.get('.input > input').type('Example task');
+    cy.get('.input > input').should('have.value', 'Example task');
+    cy.get('.btn').click();
+    cy.get('.input > input').should('have.value', '');
+
+    cy.end();
+});
