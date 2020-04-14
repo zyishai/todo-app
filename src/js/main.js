230906d3487e920e838904eba460db08e1b1f069
Yishai Zehavi <zehaviyishai@gmail.com>
2020-04-14 14:41:00 +0300

    refactor: refactor `main.js` to use `dom-adapter.js` for DOM rendering

--- src/js/main.js
+++ src/js/main.js
@@ -1,107 +1,75 @@
-import { AppState } from './state';
-import { LocalStorageWrapper } from './storage';
-
 export class Main {
-    static init() {
-        return new Main();
+    static init(domAdapter, state) {
+        return new Main(domAdapter, state);
     }
 
-    constructor() {
-        // DOM elements
-        this.newTaskInput = document.querySelector('.input input');
-        this.createNewTaskButton = document.querySelector('form .btn[type="submit"]');
-        this.tasksList = document.querySelector('.tasks ul');
-        this.createNewTaskForm = document.querySelector('.container form');
-        this.deleteAllFinishedTasksButton = document.querySelector('.tasks .clear');
+    constructor(domAdapter, state) {
+        this.domAdapter = domAdapter;
+        this.state = state;
+        this.state.setRenderer(this);
 
-        this.state = new AppState(this, new LocalStorageWrapper());
-        this.state.tasks.forEach(this.displayTask.bind(this));
+        this.registerGlobalListeners();
+        this.updateView();
+    }
+
+    _newTaskFormSubmitEventHandler(e) {
+        e.preventDefault();
+        this.state.addNewTask(this.domAdapter.getNewTaskInputValue());
+        this.domAdapter.clearNewTaskInput();
+    }
+
+    _clearFinishedTasksButtonClickEventHandler() {
+        this.state.clearAllFinishedTasks();
+    }
+
+    registerGlobalListeners() {
+        // `new task` form submittion -> add new task
+        this.domAdapter.addEventListener(
+            this.domAdapter.getNewTaskForm(),
+            'submit',
+            this._newTaskFormSubmitEventHandler.bind(this)
+        );
         
-        // register event listeners
-        this.createNewTaskForm.addEventListener('submit', (e) => {
-            e.preventDefault();
-            this.state.addNewTask(this.newTaskInput.value);
-            this.newTaskInput.value = '';
-        });
-        this.deleteAllFinishedTasksButton.addEventListener('click', () => {
-            this.state.clearAllFinishedTasks();
-        });
-    }
-
-    displayTask(task) {
-        const taskItem = document.createElement('li');
-        const taskStatusLabel = document.createElement('label');
-        taskStatusLabel.className = 'done';
-        taskStatusLabel.htmlFor = task.id;
-        taskStatusLabel.addEventListener('click', (e) => {
-            e.preventDefault();
-            this.displayMode(task.id);
-            this.state.toggleTaskState(task.id);
-        });
-        const taskStatusCheckbox = document.createElement('input');
-        taskStatusCheckbox.type = 'checkbox';
-        taskStatusCheckbox.id = task.id;
-        taskStatusCheckbox.checked = task.done;
-        const taskTextContainer = document.createElement('span');
-        taskTextContainer.className = 'text';
-        const taskTextContent = document.createElement('span');
-        taskTextContent.className = 'content';
-        taskTextContent.textContent = task.content;
-        taskTextContent.addEventListener('dblclick', () => {
-            this.editMode(task.id);
-        });
-        const taskTextDeleteSign = document.createElement('span');
-        taskTextDeleteSign.className = 'delete';
-        taskTextDeleteSign.innerHTML = '&#128465;';
-        taskTextDeleteSign.addEventListener('click', () => {
-            this.state.deleteTask(task.id);
-        });
-        const taskTextInput = document.createElement('input');
-        taskTextInput.type = 'text';
-        taskTextInput.addEventListener('dblclick', () => {
-            this.displayMode(task.id);
-            this.state.updateTaskContent(task.id, taskTextInput.value);
-        });
-
-        taskTextContainer.append(
-            taskTextContent,
-            taskTextDeleteSign,
-            taskTextInput
+        // `clear all finished tasks` button press
+        this.domAdapter.addEventListener(
+            this.domAdapter.getClearFinishedTasksButton(),
+            'click',
+            this._clearFinishedTasksButtonClickEventHandler.bind(this)
         );
-        taskItem.append(
-            taskStatusCheckbox,
-            taskStatusLabel,
-            taskTextContainer
-        );
-        this.tasksList.appendChild(taskItem);
     }
 
-    // INSTEAD OF THIS...
-    updateTask(task) {
-        const taskItemCheckbox = document.querySelector(`#${task.id}`);
-        taskItemCheckbox.checked = task.done;
-        document.querySelector(`#${task.id} ~ .text .content`).textContent = task.content;
+    _toggleTaskState(taskId) {
+        return (e) => {
+            e.preventDefault();
+            this.domAdapter.activateTaskDisplayMode(taskId);
+            this.state.toggleTaskState(taskId);
+        }
     }
 
-    removeTask(taskId) {
-        document.querySelector(`#${taskId}`).parentNode.remove();
+    _updateTaskContent(taskId) {
+        this.domAdapter.activateTaskDisplayMode(taskId);
+        this.state.updateTaskContent(taskId, this.domAdapter.getTaskInputValue(taskId));
     }
 
-    // ...DO THIS
+    _taskToDOMString(templateRenderer, task) {
+        return templateRenderer`
+            <li>
+                <input id=${task.id} type="checkbox" ?checked=${task.done} dir="auto">
+                <label for=${task.id} class="done" @click=${this._toggleTaskState(task.id)}></label>
+                <span class="text">
+                    <span class="content" @dblclick=${() => this.domAdapter.activateTaskEditMode(task.id)}>${task.content}</span>
+                    <span class="delete" @click=${() => this.state.deleteTask(task.id)}>&#128465;</span>
+                    <input type="text" class="" @dblclick=${() => this._updateTaskContent(task.id)} value=${task.content}>
+                </span>
+            </li>
+        `;
+    }
+
     updateView() {
-        this.tasksList.innerHTML = '';
-        this.state.tasks.forEach(this.displayTask.bind(this));
-    }
-
-    editMode(taskId) {
-        const task = this.state.getById(taskId);
-        const taskTextInput = document.querySelector(`#${taskId} ~ .text input`);
-        taskTextInput.classList.add('active');
-        taskTextInput.value = task.content;
-    }
-
-    displayMode(taskId) {
-        const taskTextInput = document.querySelector(`#${taskId} ~ .text input`);
-        taskTextInput.classList.remove('active');
+        this.domAdapter.renderListToContainer(
+            this.state.tasks, 
+            this._taskToDOMString.bind(this),
+            this.domAdapter.getTasksList()
+        );
     }
 }
