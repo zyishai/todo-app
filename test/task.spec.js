c0a8ae3f3ed7f810be1d217dbc0958c46db15612
Yishai Zehavi <zehaviyishai@gmail.com>
2020-04-17 16:03:18 +0300

    test(task): added test to check initialization of `done` field

--- test/task.spec.js
+++ test/task.spec.js
@@ -1,22 +1,34 @@
 import { expect } from 'chai';
 import { Task } from '../src/js/task';
 
-test('when creating an empty task, the `id` field should not be empty', () => {
+test('when creating an empty task, the `id` field should not be empty (random)', () => {
     const task = new Task();
-
+    
     expect(task.id).to.not.be.null;
     expect(task.id).to.not.be.undefined;
 });
-
-test('when creating a task with `id`, the `id` field should initialize to that value', () => {
-    const task = new Task('0', 'Hello world');
-
-    expect(task.id).to.be.equal('0');
-    expect(task.content).to.be.equal('Hello world');
-})
 
 test('when creating a task, the `done` field should be initialized to false', () => {
     const task = new Task();
 
     expect(task.done).to.be.eq(false);
+});
+
+test('when creating a task with one argument, the `content` field should be initialized to that value', () => {
+    const task = new Task('Hello world');
+
+    expect(task.content).to.be.equal('Hello world');
+})
+
+test('when creating a task with two arguments, the `id` field should initialize to the first value', () => {
+    const task = new Task('0', 'Hello world');
+
+    expect(task.id).to.be.equal('0');
+    expect(task.content).to.be.equal('Hello world');
+});
+
+test('when creating a task with three arguments, the `done` field should be initialized to the third argument', () => {
+    const task = new Task('0', 'Hello world', true);
+
+    expect(task.done).to.be.true;
 });
