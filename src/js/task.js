0df69fff7d5810613e2e16fa008ecd2805bad046
Yishai Zehavi <zehaviyishai@gmail.com>
2020-04-17 16:04:30 +0300

    refactor(task): refactor `task.js` to provide dynamic constructor

--- src/js/task.js
+++ src/js/task.js
@@ -1,14 +1,27 @@
 import { randomBytes } from 'crypto';
 
 export class Task {
-    constructor(text) {
-        this.content = text;
-        this.done = false;
-        this.generateId();
+    constructor() {
+        if (arguments.length >= 3) {
+            this.id = String(arguments[0]);
+            this.content = String(arguments[1]);
+            this.done = Boolean(arguments[2]);
+        } else if (arguments.length === 2) { // Task('abcd', 'some message')
+            this.id = String(arguments[0]);
+            this.content = String(arguments[1]);
+            this.done = false;
+        } else if (arguments.length === 1) { // Task('Hi')
+            this.content = String(arguments[0]);
+            this.id = this.generateId();
+            this.done = false;
+        } else {
+            this.id = this.generateId(); // Task()
+            this.done = false;
+        }
     }
 
     generateId() {
-        this.id = 'task-' + randomBytes(6).toString('hex');
+        return 'task-' + randomBytes(6).toString('hex');
     }
 
     toJSON() {
