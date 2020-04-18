6aec47d85f903620243126c51ee38b4c68b08fe1
Yishai Zehavi <zehaviyishai@gmail.com>
2020-04-18 22:05:10 +0300

    refactor(task): added methods to set the content and toggle the state of the task

--- src/js/task.js
+++ src/js/task.js
@@ -1,27 +1,37 @@
 import { randomBytes } from 'crypto';
 
 export class Task {
-    constructor() {
-        if (arguments.length >= 3) {
-            this.id = String(arguments[0]);
-            this.content = String(arguments[1]);
-            this.done = Boolean(arguments[2]);
-        } else if (arguments.length === 2) { // Task('abcd', 'some message')
-            this.id = String(arguments[0]);
-            this.content = String(arguments[1]);
+    static empty = null;
+
+    constructor(...args) {
+        if (args.length >= 3) {
+            this.id = String(args[0]);
+            this.content = String(args[1]);
+            this.done = Boolean(args[2]);
+        } else if (args.length === 2) { // Task('abcd', 'some message')
+            this.id = String(args[0]);
+            this.content = String(args[1]);
             this.done = false;
-        } else if (arguments.length === 1) { // Task('Hi')
-            this.content = String(arguments[0]);
-            this.id = this.generateId();
+        } else if (args.length === 1) { // Task('Hi')
+            this.content = String(args[0]);
+            this.id = this._generateId();
             this.done = false;
         } else {
-            this.id = this.generateId(); // Task()
+            this.id = this._generateId(); // Task()
             this.done = false;
         }
     }
 
-    generateId() {
+    _generateId() {
         return 'task-' + randomBytes(6).toString('hex');
+    }
+
+    toggleState() {
+        this.done = !this.done;
+    }
+
+    setContent(text) {
+        this.content = text;
     }
 
     toJSON() {
