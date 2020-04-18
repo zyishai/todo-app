2784d975658b2ed8c630a144efd13df8de4055bf
Yishai Zehavi <zehaviyishai@gmail.com>
2020-04-18 22:59:28 +0300

    test(state): fixing tests to support async/await notion and testing the event methods

--- test/state.spec.js
+++ test/state.spec.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { stub, spy } from 'sinon';
 import { AppState } from '../src/js/state';
 import AppStateEvents from '../src/js/app-state-events';
-import { LocalStorageWrapper } from '../src/js/storage';
+import { StorageAdapter } from '../src/js/storage';
 
 suiteSetup(function() {
     let store = {};
@@ -23,42 +23,53 @@ suiteSetup(function() {
 });
 
 setup(function() {
-    this.state = new AppState(new LocalStorageWrapper());
-    spy(this.state);
+    // make sure that the tasks got loaded from the storage before
+    // starting the tests.
+    return new Promise(resolve => {
+        this.state = new AppState(new StorageAdapter(''));
+        spy(this.state);
+        this.state.onTasksSyncEvent(resolve);
+    });
 });
 
-test('initial state should come from `storage`', function() {
+test('initial state should come from `storage`', async function() {
     spy(this.state.storage);
-    expect(this.state.tasks).to.deep.equal(this.state.storage.fetchAllTasks());
+    expect(this.state.tasks).to.deep.equal(await this.state.storage.fetchAllTasks());
 });
 
-test('calling addNewTask() should save the task in the state', function() {
-    this.state.addNewTask('Example task');
+test('calling addNewTask() should save the task in the state', async function() {
+    await this.state.addNewTask('Example task');
 
     expect(this.state.tasks).to.be.lengthOf(1);
 });
 
-test('calling addNewTask() should call storage.persistTask()', function() {
+test('calling addNewTask() should call storage.persistTask()', async function() {
     spy(this.state.storage);
-    this.state.addNewTask('Example task');
+    await this.state.addNewTask('Example task');
 
     expect(this.state.storage.persistTask).to.be.called;
 });
 
-test('calling addNewTask() should emit `new task` event', function() {
+test('calling addNewTask() should emit `new task` event', async function() {
     // act
-    const task = this.state.addNewTask('Example task');
+    const task = await this.state.addNewTask('Example task');
 
     // assert
-    expect(this.state.emit).to.be.calledWith(AppStateEvents.NEW_TASK_EVENT, task);
+    expect(this.state.emit).to.be.calledWith(AppStateEvents.NEW_TASK_ADDED_EVENT, task);
 });
 
-test('calling toggleTaskState() should toggle `task.done` and emit `task state changed` event with the task id, prev state and new state', function() {
-    const task = this.state.addNewTask('Example');
+test('calling addNewTask() should emit `sync` event', async function() {
+    await this.state.addNewTask('test');
 
-    this.state.toggleTaskState(task.id);
+    expect(this.state.emit).to.be.calledWith(AppStateEvents.TASKS_SYNC);
+});
 
-    expect(task.done).to.be.true;
+test('calling toggleTaskState() should toggle `task.done` and emit `task state changed` event with the task id, prev state and new state', async function() {
+    const task = await this.state.addNewTask('Example');
+
+    await this.state.toggleTaskState(task.id);
+
+    expect(this.state._getById(task.id).done).to.be.true;
     expect(this.state.emit).to.be.deep.calledWith(AppStateEvents.TASK_STATE_CHANGED_EVENT, {
         taskId: task.id,
         prevState: false,
@@ -66,26 +77,33 @@ test('calling toggleTaskState() should toggle `task.done` and emit `task state c
     });
 
     // should return task.done to be false
-    this.state.toggleTaskState(task.id);
+    await this.state.toggleTaskState(task.id);
 
     expect(task.done).to.be.false;
 });
 
-test('calling toggleTaskState() should call storage.updateTask', function() {
+test('calling toggleTaskState() should call storage.updateTask', async function() {
     spy(this.state.storage);
-    const task = this.state.addNewTask('Example task');
+    const task = await this.state.addNewTask('Example task');
 
-    this.state.toggleTaskState(task.id);
+    await this.state.toggleTaskState(task.id);
 
     expect(this.state.storage.updateTask).to.be.called;
 });
 
-test('calling updateTaskContent() should set task\'s content property and emit `task content update` event', function() {
-    const task = this.state.addNewTask('Example');
+test('calling toggleTaskState() should emit `sync` event', async function() {
+    const task = await this.state.addNewTask('test');
+    await this.state.toggleTaskState(task.id);
 
-    this.state.updateTaskContent(task.id, 'Two words');
+    expect(this.state.emit).to.be.calledWith(AppStateEvents.TASKS_SYNC);
+});
 
-    expect(task.content).to.equal('Two words');
+test('calling updateTaskContent() should set task\'s content property and emit `task content update` event', async function() {
+    const task = await this.state.addNewTask('Example');
+
+    await this.state.updateTaskContent(task.id, 'Two words');
+
+    expect(this.state._getById(task.id).content).to.equal('Two words');
     expect(this.state.emit).to.be.deep.calledWith(AppStateEvents.TASK_CONTENT_UPDATED_EVENT, {
         taskId: task.id,
         prevContent: 'Example',
@@ -93,48 +111,62 @@ test('calling updateTaskContent() should set task\'s content property and emit `
     });
 });
 
-test('calling updateTaskContent() should call storage.updateTask', function() {
+test('calling updateTaskContent() should call storage.updateTask', async function() {
     spy(this.state.storage);
-    const task = this.state.addNewTask('Example task');
+    const task = await this.state.addNewTask('Example task');
 
-    this.state.updateTaskContent(task.id, 'Two words');
+    await this.state.updateTaskContent(task.id, 'Two words');
 
     expect(this.state.storage.updateTask).to.be.called;
 });
 
-test('when calling deleteTask() the task should be removed from state', function() {
-    const task = this.state.addNewTask('Test');
+test('calling updateTaskContent() should emit `sync` event', async function() {
+    const task = await this.state.addNewTask('test');
+    await this.state.updateTaskContent(task.id, 'Hello world');
 
-    this.state.deleteTask(task.id);
+    expect(this.state.emit).to.be.calledWith(AppStateEvents.TASKS_SYNC);
+});
+
+test('when calling deleteTask() the task should be removed from state', async function() {
+    const task = await this.state.addNewTask('Test');
+
+    await this.state.deleteTask(task.id);
 
     expect(this.state.tasks).to.not.contain(task);
 });
 
-test('when calling deleteTask(), storage.deleteTask() should be called', function() {
+test('when calling deleteTask(), storage.deleteTask() should be called', async function() {
     spy(this.state.storage);
-    const task = this.state.addNewTask('Test');
+    const task = await this.state.addNewTask('Test');
 
-    this.state.deleteTask(task.id);
+    await this.state.deleteTask(task.id);
 
     expect(this.state.storage.deleteTask).to.be.calledWith(task.id);
 });
 
-test('when calling deleteTask(), `tasks deleted` event should be emitted', function() {
-    const task = this.state.addNewTask('Test');
+test('when calling deleteTask(), `tasks deleted` event should be emitted', async function() {
+    const task = await this.state.addNewTask('Test');
 
-    this.state.deleteTask(task.id);
+    await this.state.deleteTask(task.id);
 
     expect(this.state.emit).to.be.deep.calledWith(AppStateEvents.TASKS_DELETED_EVENT, [
         task.id
     ]);
 });
 
-test('calling clearAllFinishedTasks() should remove all finished tasks', function() {
-    this.state.addNewTask('First task');
-    const toBeClearedTask = this.state.addNewTask('Second task');
-    this.state.toggleTaskState(toBeClearedTask.id);
-    this.state.addNewTask('Third task');
-    this.state.clearAllFinishedTasks();
+test('calling deleteTask() should emit `sync` event', async function() {
+    const task = await this.state.addNewTask('test');
+    await this.state.deleteTask(task.id);
+
+    expect(this.state.emit).to.be.calledWith(AppStateEvents.TASKS_SYNC);
+});
+
+test('calling clearAllFinishedTasks() should remove all finished tasks', async function() {
+    await this.state.addNewTask('First task');
+    const toBeClearedTask = await this.state.addNewTask('Second task');
+    await this.state.toggleTaskState(toBeClearedTask.id);
+    await this.state.addNewTask('Third task');
+    await this.state.clearAllFinishedTasks();
 
     expect(this.state.tasks)
         .to.have.lengthOf(2)
@@ -142,21 +174,75 @@ test('calling clearAllFinishedTasks() should remove all finished tasks', functio
         .to.not.deep.include(toBeClearedTask.toJSON());
 });
 
-test('calling clearAllFinishedTasks() should call storage.clearAllFinishedTasks()', function() {
+test('calling clearAllFinishedTasks() should call storage.clearAllFinishedTasks()', async function() {
     spy(this.state.storage);
-    this.state.addNewTask('First task');
-    this.state.addNewTask('Second task');
-    this.state.addNewTask('Third task');
-    this.state.clearAllFinishedTasks();
+    await this.state.addNewTask('First task');
+    await this.state.addNewTask('Second task');
+    await this.state.addNewTask('Third task');
+    await this.state.clearAllFinishedTasks();
 
     expect(this.state.storage.clearAllFinishedTasks).to.be.called;
 });
 
-test('calling clearAllFinishedTasks() should emit `tasks deleted` event', function() {
-    this.state.addNewTask('First task');
-    this.state.addNewTask('Second task');
-    this.state.addNewTask('Third task');
-    this.state.clearAllFinishedTasks();
+test('calling clearAllFinishedTasks() should emit `tasks deleted` event', async function() {
+    await this.state.addNewTask('First task');
+    await this.state.addNewTask('Second task');
+    await this.state.addNewTask('Third task');
+    await this.state.clearAllFinishedTasks();
 
     expect(this.state.emit).to.be.deep.calledWith(AppStateEvents.TASKS_DELETED_EVENT);
+});
+
+test('calling clearAllFinishedTasks() should emit `sync` event', async function() {
+    await this.state.clearAllFinishedTasks();
+
+    expect(this.state.emit).to.be.calledWith(AppStateEvents.TASKS_SYNC);
+});
+
+test('calling onTasksSyncEvent() should registered listener for `sync` events', function() {
+    this.state.onTasksSyncEvent(() => {
+        expect(true).to.be.true;
+    });
+
+    this.state.emit(AppStateEvents.TASKS_SYNC);
+});
+
+test('calling onNewTaskEvent() should registered listener for `new event` events', function() {
+    this.state.onNewTaskEvent(() => {
+        expect(true).to.be.true;
+    });
+
+    this.state.emit(AppStateEvents.NEW_TASK_ADDED_EVENT);
+});
+
+test('calling onTaskStateChangedEvent() should registered listener for `task state changed` events', function() {
+    this.state.onTaskStateChangedEvent(() => {
+        expect(true).to.be.true;
+    });
+
+    this.state.emit(AppStateEvents.TASK_STATE_CHANGED_EVENT);
+});
+
+test('calling onTaskContentUpdatedEvent() should registered listener for `task content changed` events', function() {
+    this.state.onTaskContentUpdatedEvent(() => {
+        expect(true).to.be.true;
+    });
+
+    this.state.emit(AppStateEvents.TASK_CONTENT_UPDATED_EVENT);
+});
+
+test('calling onSingleTaskDeletedEvent() should registered listener for `tasks deleted` events', function() {
+    this.state.onSingleTaskDeletedEvent(() => {
+        expect(true).to.be.true;
+    });
+
+    this.state.emit(AppStateEvents.TASKS_DELETED_EVENT);
+});
+
+test('calling onMultipleTasksDeletedEvent() should registered listener for `tasks deleted` events', function() {
+    this.state.onMultipleTasksDeletedEvent(() => {
+        expect(true).to.be.true;
+    });
+
+    this.state.emit(AppStateEvents.TASKS_DELETED_EVENT);
 });
