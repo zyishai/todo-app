cb580d1f78abf345a0f9c72e7c653eb6e683a16f
Yishai Zehavi <zehaviyishai@gmail.com>
2020-04-19 20:06:39 +0300

    refactor: extracted the process of construction of the storage url to a separate method

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,21 @@
-require('dotenv').config();
 import { Main } from './main';
 import litHtmlAdapter from './dom-adapter';
 import { AppState } from './state';
 import { StorageAdapter } from './storage';
 
 ;(function() {
-    const appState = new AppState(new StorageAdapter(process.env.REMOTE_STORAGE_URL));
+    function constructRemoteStorageUrl() {
+        const username = process.env.REMOTE_STORAGE_USERNAME;
+        const password = process.env.REMOTE_STORAGE_PASSWORD;
+        const host = process.env.REMOTE_STORAGE_URL_HOST;
+        const databaseName = process.env.REMOTE_STORAGE_DB_NAME;
+        return host 
+            ? username && password
+                ? `http://${username}:${password}@${host}/${databaseName}`
+                : `http://${host}/${databaseName}`
+            : databaseName;
+
+    }
+    const appState = new AppState(new StorageAdapter(constructRemoteStorageUrl()));
     Main.init(litHtmlAdapter, appState);
 })()
