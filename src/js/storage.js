34f38280739f33ce16d7a9406848e83acb52f7fc
Yishai Zehavi <zehaviyishai@gmail.com>
2020-04-19 14:18:06 +0300

    refactor(storage): replaced local storage with remote storage (CouchDB)

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,27 +1,56 @@
-export class LocalStorageWrapper {
-    persistTask(task) {
-        const allTasks = this.fetchAllTasks();
-        allTasks.push(task.toJSON());
-        localStorage.setItem('tasks', JSON.stringify(allTasks));
+import PouchDB from 'pouchdb';
+import { Task } from './task';
+
+export class StorageAdapter {
+    constructor(url) {
+        this.db = new PouchDB(url);
     }
 
-    updateTask(task) {
-        const tasks = this.fetchAllTasks().map(t => t.id === task.id ? task : t);
-        localStorage.setItem('tasks', JSON.stringify(tasks));
+    async _getDocById(id) {
+        return await this.db.get(id);
     }
 
-    fetchAllTasks() {
-        return JSON.parse(localStorage.getItem('tasks') || '[]');
+    async _getAllDocs(opts) {
+        return await this.db.allDocs({
+            include_docs: true,
+            ...opts
+        });
     }
 
-    deleteTask(taskId) {
-        let tasks = this.fetchAllTasks().filter(t => t.id !== taskId);
-
-        localStorage.setItem('tasks', JSON.stringify(tasks));
+    async fetchAllTasks() {
+        const allDocs = await this._getAllDocs();
+        return allDocs.rows.map(({ doc }) => new Task(doc.id, doc.content, doc.done));
     }
 
-    clearAllFinishedTasks() {
-        const tasks = this.fetchAllTasks().filter(task => !task.done);
-        localStorage.setItem('tasks', JSON.stringify(tasks));
+    async persistTask(task) {
+        return await this.db.put({
+            _id: task.id,
+            ...task.toJSON()
+        });
+    }
+
+    async updateTask(task) {
+        const { _rev } = await this.db.get(task.id);
+        return await this.db.put({
+            _id: task.id,
+            _rev,
+            ...task.toJSON()
+        });
+    }
+
+    async deleteTask(taskId) {
+        const taskDoc = await this._getDocById(taskId);
+        return await this.db.remove(taskDoc.id, taskDoc._rev);
+    }
+
+    async clearAllFinishedTasks() {
+        const allTaskRows = (await this._getAllDocs()).rows;
+        return Promise.all(allTaskRows.map(row => {
+            if (row.doc.done) {
+                return this.db.remove(row.doc.id, row.value.rev);
+            } else {
+                return Promise.resolve();
+            }
+        }));
     }
 }
