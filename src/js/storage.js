e1982cc35c5aea3742763bb7f6bf1d9777a02f46
Yishai Zehavi <zehaviyishai@gmail.com>
2020-04-19 21:09:18 +0300

    fix(storage): installed couchdb-find plugin to support Mongo-like `find()` operator
    when CouchDB saves a document, it assign it an `_id` field which it generates, which is different
    than the `id` field that I'm generating when I'm creating a task. When `updateTask` is called, it
    gets the task's id but CouchDB needs its `_id` field which is not present on the task, so I need to
    make a Mongo-like query (`find`) to find my task and retrieve its `_id` field and couchdb-find is a
    plugin which support this kind of queries.

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,13 +1,20 @@
 import PouchDB from 'pouchdb';
+import PouchDBFind from 'pouchdb-find';
 import { Task } from './task';
+
+PouchDB.plugin(PouchDBFind);
 
 export class StorageAdapter {
     constructor(url) {
         this.db = new PouchDB(url);
     }
 
-    async _getDocById(id) {
-        return await this.db.get(id);
+    async _getDocById(taskId) {
+        return await this.db.find({
+            selector: {
+                id: taskId
+            }
+        }).then(res => res.docs[0]);
     }
 
     async _getAllDocs(opts) {
@@ -29,16 +36,13 @@ export class StorageAdapter {
     }
 
     async persistTask(task) {
-        return await this.db.put({
-            _id: task.id,
-            ...task.toJSON()
-        });
+        return await this.db.post(task.toJSON());
     }
 
     async updateTask(task) {
-        const { _rev } = await this.db.get(task.id);
+        const { _rev, _id } = await this._getDocById(task.id);
         return await this.db.put({
-            _id: task.id,
+            _id,
             _rev,
             ...task.toJSON()
         });
@@ -46,14 +50,14 @@ export class StorageAdapter {
 
     async deleteTask(taskId) {
         const taskDoc = await this._getDocById(taskId);
-        return await this.db.remove(taskDoc.id, taskDoc._rev);
+        return await this.db.remove(taskDoc._id, taskDoc._rev);
     }
 
     async clearAllFinishedTasks() {
         const allTaskRows = (await this._getAllDocs()).rows;
         return Promise.all(allTaskRows.map(row => {
             if (row.doc.done) {
-                return this.db.remove(row.doc.id, row.value.rev);
+                return this.db.remove(row.doc._id, row.value.rev);
             } else {
                 return Promise.resolve();
             }
